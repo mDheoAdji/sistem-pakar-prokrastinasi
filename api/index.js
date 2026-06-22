b19e1ddc4c6c959834b23806b716f147c1be8473
mDheoAdji <m.dheo008@gmail.com>
2026-06-22 19:00:28 +0700

    prepare vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
 
-const diagnosisRoute = require("./routes/diagnosis");
-const symptomsRoute = require("./routes/symptoms");
+const diagnosisRoute = require("../routes/diagnosis");
+const symptomsRoute = require("../routes/symptoms");
 
 const app = express();
 
@@ -17,8 +16,4 @@ app.get("/", (req, res) => {
   res.send("Sistem Pakar Prokrastinasi Akademik");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
