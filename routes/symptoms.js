177e23f2e510ce14c4e4c255aab726b634721f89
mDheoAdji <m.dheo008@gmail.com>
2026-07-05 20:05:56 +0700

    f commit

--- routes/symptoms.js
+++ routes/symptoms.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const symptoms = require("../data/symptoms.json");
 
+console.log("SYMPTOMS YANG DIBACA:");
+console.log(symptoms);
+
 router.get("/", (req, res) => {
   res.json(symptoms);
 });
